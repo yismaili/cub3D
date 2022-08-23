8ff0f8340a3c51a575ca6583df6ae5f57ef1cc14
Younes Ismaili <yismaili@e3r5p4.1337.ma>
2022-08-23 14:43:54 +0100

    parsing NO SO WE EA F C

--- rsc/errors_handling.c
+++ rsc/errors_handling.c
@@ -6,7 +6,7 @@
 /*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 21:24:03 by yismaili          #+#    #+#             */
-/*   Updated: 2022/08/23 10:26:52 by yismaili         ###   ########.fr       */
+/*   Updated: 2022/08/23 14:39:41 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,8 @@ int ft_check_texture(t_struct *cub, char *dirct, int len)
 
 int ft_check_alltextures(t_struct *cub)
 {
+    if (ft_check_alldouble(cub) == 0)
+        return (ft_putstr_fd("invalide map\n", 2), 0);
     if (ft_check_texture(cub, "NO", 2) == 0)
         return (ft_putstr_fd("North texureh not fount\n", 2), 0);
     if (ft_check_texture(cub, "SO", 2) == 0)
@@ -62,7 +64,7 @@ int ft_check_alltextures(t_struct *cub)
     return (1);
 }
 
-int ft_check_florclg(t_struct *cub, char *flor_clg, int len)
+char    **ft_check_florclg(t_struct *cub, char *flor_clg, int len)
 {
     char    *data;
     char    *ptr;
@@ -72,7 +74,7 @@ int ft_check_florclg(t_struct *cub, char *flor_clg, int len)
 
     data = ft_search_inmap(cub, flor_clg, len);
     if (!data)
-        return (0);
+        return (NULL);
     ptr = data;
     data = ft_substr(data, len + 1, (ft_strlen(data) -(len + 2)));
     free(ptr);
@@ -81,9 +83,66 @@ int ft_check_florclg(t_struct *cub, char *flor_clg, int len)
     while (splt_data[i])
     {
         num = ft_atoi(splt_data[i]);
-        if (num == -45)
-           return (0);
+        if (num == -45 || (num < 0 || num > 255))
+           return (NULL);
        i++ ;
     }
+    if (i < 3 || i > 3)
+        return (NULL);
+    return (splt_data);
+}
+
+int ft_check_rgb(t_struct *cub)
+{
+    char    **splt_dataflr;
+    char    **splt_dataclg;
+
+    if (ft_check_alldouble(cub) == 0)
+         return (0);
+    splt_dataflr = ft_check_florclg(cub, "F", 1);
+    if (splt_dataflr != NULL)
+    {
+        cub->flr.r = ft_atoi(splt_dataflr[0]);
+        cub->flr.g = ft_atoi(splt_dataflr[1]);
+        cub->flr.b = ft_atoi(splt_dataflr[2]);
+    }
+    else
+        return (ft_putstr_fd("invalide color\n", 2), 0);
+    splt_dataclg = ft_check_florclg(cub, "C", 1);
+    if (splt_dataclg != NULL)
+    {
+        cub->clg.r = ft_atoi(splt_dataclg[0]);
+        cub->clg.g = ft_atoi(splt_dataclg[1]);
+        cub->clg.b = ft_atoi(splt_dataclg[2]);
+    }
+    else
+        return (ft_putstr_fd("invalide color\n", 2), 0);
+    return (1);
+}
+
+int ft_check_double(t_struct *cub, char *dirct, int len)
+{
+    int cunt;
+    int i;
+
+    i = 0;
+    cunt = 0;
+    while (cub->map[i])
+    {
+        if (!ft_strncmp(cub->map[i], dirct, len))
+            cunt++;
+        i++;
+    }
+    return (cunt);
+}
+
+int ft_check_alldouble(t_struct *cub)
+{
+    if (ft_check_double(cub, "NO", 2) == 2 || ft_check_double(cub, "SO", 2) == 2)
+        return (0);
+    if (ft_check_double(cub, "WE", 2) == 2 || ft_check_double(cub, "EA", 2) == 2)
+        return (0);
+    if (ft_check_double(cub, "F", 1) == 2 || ft_check_double(cub, "C", 1) == 2)
+        return (0);  
     return (1);
 }
