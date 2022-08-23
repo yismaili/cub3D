f521243dd31ce6d2e91fc50005c2872f8756d982
Younes Ismaili <yismaili@e3r5p4.1337.ma>
2022-08-23 15:37:29 +0100

    parsing NO SO WE EA F C

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 13:56:15 by yismaili          #+#    #+#             */
-/*   Updated: 2022/08/23 13:11:50 by yismaili         ###   ########.fr       */
+/*   Updated: 2022/08/23 15:25:27 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,20 @@ typedef struct s_ceilling
 	int b;
 }	t_ceilling;
 
+typedef struct s_dirct
+{
+	char *north_path;
+	char *south_path;
+	char *west_path;
+	char *east_path;
+}	t_dirct;
+
 typedef struct s_struct
 {
 	char		**map;
 	t_ceilling 	clg;
 	t_floor		flr;
+	t_dirct		drct;
 }	t_struct;
 
 int     get_height(char *map_file);
@@ -45,11 +54,12 @@ int     get_width(char *map_file);
 char    *ft_remplir_map(char *map, char *line);
 char	*get_next_line(int fd);
 void	ft_read_maps(char *map_file, t_struct *ptr);
-int		ft_check_texture(t_struct *cub, char *dirct, int len);
+char    *ft_check_texture(t_struct *cub, char *dirct, int len);
 char    *ft_search_inmap(t_struct *cub, char *search, int len_ofsrch);
 int		ft_check_alltextures(t_struct *cub);
 char    **ft_check_florclg(t_struct *cub, char *flor_clg, int len);
 int		ft_check_rgb(t_struct *cub);
 int		ft_check_double(t_struct *cub, char *dirct, int len);
 int		ft_check_alldouble(t_struct *cub);
+
 #endif
