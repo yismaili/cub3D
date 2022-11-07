b8cc95ba222e5deb082fe83c13972aef0fd66e2c
Younes Ismaili <yismaili@e2r5p1.1337.ma>
2022-11-07 16:49:39 +0100

    player moves

--- raycaster/draw_map.c
+++ raycaster/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: yismaili < yismaili@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 12:48:33 by yismaili          #+#    #+#             */
-/*   Updated: 2022/11/07 13:55:38 by yismaili         ###   ########.fr       */
+/*   Updated: 2022/11/07 16:43:27 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,6 +84,7 @@ void    ft_draw_map(t_struct *cub)
     y = 0;
     len = 0;
     data = ft_jump_lines(cub);
+    player_position(cub);
     while (data[y])
     {
         x = 0;
@@ -102,3 +103,42 @@ void    ft_draw_map(t_struct *cub)
     }
     mlx_put_image_to_window(cub->mlx_ptr, cub->win_ptr, cub->img, 1, 1);
 }
+
+void player_position(t_struct *cub){
+    int i = 0;
+    int j = 0;
+   char** data = ft_jump_lines(cub);
+   
+   while (data[i])
+   {
+    j = 0;
+    while(data[i][j]){
+        if (data[i][j] == 'E'|| data[i][j] == 'N' || data[i][j] == 'S' || data[i][j] == 'W'){
+         cub->player.position_x = j;
+         cub->player.position_y = i;
+        return ;
+        }
+        j++;
+    }
+    i++;
+   } 
+}
+
+int	player_move(int key, t_struct *p)
+{
+	if (key == 125)
+		p->player.position_y += 10;
+	if (key == 126)
+		p->player.position_y -= 10;
+	if (key == 124)
+		p->player.position_y += 10;
+	if (key == 123)
+		p->player.position_y -= 10;
+	return (0);
+    mlx_destroy_image(p->mlx_ptr, p->img);
+    p->img = mlx_new_image(p->mlx_ptr, W_WIDTH, W_HEIGHT);
+    ft_draw_map(p);
+}
+// void update_ptayer(t_struct *cub){
+    
+// }
