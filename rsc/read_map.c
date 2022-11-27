bf096dca29f0851c9fb021ef018301a78291d87d
Younes Ismaili <yismaili@e2r6p1.1337.ma>
2022-11-27 22:12:36 +0100

    fix  norminette in read map file

--- rsc/read_map.c
+++ rsc/read_map.c
@@ -6,36 +6,37 @@
 /*   By: yismaili < yismaili@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 13:53:11 by yismaili          #+#    #+#             */
-/*   Updated: 2022/11/27 20:44:00 by yismaili         ###   ########.fr       */
+/*   Updated: 2022/11/27 21:29:59 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    int 	i;
-    int 	read_line;
-    char	c;
-    char 	*buff;
-	
+	int		i;
+	int		read_line;
+	char	c;
+	char	*buff;
+
 	i = 0;
-    read_line = 1;
+	read_line = 1;
 	buff = (char *)ft_calloc(sizeof(char), BUFF);
-    while ((read_line = read(fd, &c, 1)) > 0)
-    {
-        if (c == '\n')
-            break;
-        buff[i] = c;
+	while (read_line > 0)
+	{
+		read_line = read(fd, &c, 1);
+		if (c == '\n')
+			break ;
+		buff[i] = c;
 		i++;
 	}
-    buff[i] =  '\0';
-    if ((!buff[i] && !read_line) || read_line == - 1)
-    {
-        free(buff);
-        return (NULL);
-    }
-    return(buff);
+	buff[i] = '\0';
+	if ((!buff[i] && !read_line) || read_line == -1)
+	{
+		free(buff);
+		return (NULL);
+	}
+	return (buff);
 }
 
 int	get_height(char *map_file)
@@ -46,10 +47,10 @@ int	get_height(char *map_file)
 
 	fd = open(map_file, O_RDONLY);
 	if (fd < 0)
-    {
+	{
 		ft_putstr_fd("Error Open file\n", 1);
-        return (0);
-    }
+		return (0);
+	}
 	height = 0;
 	get_line = get_next_line(fd);
 	while (get_line)
@@ -67,17 +68,18 @@ int	get_width(char *map_file, int height)
 	int		fd;
 	int		width;
 	char	*get_line;
-	int i = 0;
-	size_t max;
+	int		i;
+	size_t	max;
 
 	width = 0;
+	i = 0;
 	max = 0;
 	fd = open (map_file, O_RDONLY);
 	if (fd < 0)
-    {
+	{
 		ft_putstr_fd("Error Open file\n", 1);
-        exit (0);
-    }
+		exit (0);
+	}
 	while (i < height)
 	{
 		get_line = get_next_line(fd);
@@ -99,21 +101,21 @@ int	ft_read_maps(char *map_file, t_struct *cub)
 	i = 0;
 	fd = open(map_file, O_RDONLY);
 	if (fd < 0)
-    {
+	{
 		ft_putstr_fd("Error Open file\n", 1);
-        return (0);
-    }
-    cub->height = get_height(map_file);
+		return (0);
+	}
+	cub->height = get_height(map_file);
 	cub->width = get_width(map_file, cub->height);
-	cub->map = (char **)ft_calloc(sizeof(char *), (cub->height + 1));
+	cub->map = (char **)ft_calloc(sizeof(char *), (cub->height +1));
 	while (i < cub->height)
 	{
 		get_line = get_next_line(fd);
-        cub->map[i] = ft_strdup(get_line);
+		cub->map[i] = ft_strdup(get_line);
 		free(get_line);
 		i++;
 	}
-  	cub->map[i] = NULL;
+	cub->map[i] = NULL;
 	close(fd);
 	return (1);
 }
