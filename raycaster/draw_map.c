fc9422929a8b9b4369003aac1f57a41db84d3e41
Younes Ismaili <yismaili@e2r5p1.1337.ma>
2022-11-28 22:42:33 +0100

    fix  norminette in rsc dir

--- raycaster/draw_map.c
+++ raycaster/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: yismaili < yismaili@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 12:48:33 by yismaili          #+#    #+#             */
-/*   Updated: 2022/11/27 18:27:26 by yismaili         ###   ########.fr       */
+/*   Updated: 2022/11/28 22:37:48 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -204,4 +204,95 @@ void directionOfPlayer(t_struct *cub)
         cub->player.rottAngle = M_PI;
     if (cub->my_map[gred_y][gred_x] == 'E')
         cub->player.rottAngle = 0;
+}
+
+int	ft_close(int keycode)
+{
+	if (keycode)
+		exit(1);
+	return (0);
+}
+
+int	motion_notify(int x, int y, t_struct *cub)
+{
+	if (x > 0 && x < W_WIDTH && y > 0 && y < W_HEIGHT)
+	{
+		if (cub->med_x < x)
+			cub->player.rottAngle += cub->player.rottSpeed / 6;
+		if (cub->med_x > x)
+			cub->player.rottAngle -= cub->player.rottSpeed / 6;
+		if (cub->med_x == x)
+			cub->player.rottAngle = cub->player.rottAngle;
+		cub->med_x = x;
+	}
+	return (0);
+}
+
+
+void	color_oftexture(t_struct *cub)
+{
+	int			i;
+	int			j;
+	
+	i = -1;
+	while (++i < W_HEIGHT)
+		cub->color_buffer[i] = (unsigned int *)ft_calloc(sizeof(unsigned int), \
+				W_WIDTH);
+	ft_colorBuffer(cub);
+	cub->wallTexture = (unsigned int *) ft_calloc (sizeof(unsigned int), \
+			(unsigned int)cub->scaleWidth * (unsigned int)cub->scaleHeight);
+	i = 0;
+	while (i < cub->scaleWidth)
+	{
+		j = 0;
+		while (j < cub->scaleHeight)
+		{
+			if (i % 8 && j % 8)
+				cub->wallTexture[(cub->scaleWidth * j) + i] = 0xadd8e6;
+			else
+				cub->wallTexture[(cub->scaleWidth * j) + i] = 0xFFF0000;
+			j++;
+		}
+		i++;
+	}
+}
+void	init_textures(t_struct *cub)
+{
+	cub->texture = malloc(sizeof(t_textures) * 4);
+	load_texture(cub, cub->texture);
+}
+
+void	load_texture(t_struct *c, t_textures *t)
+{
+	t[0].img = mlx_xpm_file_to_image(c->mlx_ptr, c->drct.north_path, \
+			&t[0].img_width, &t[0].img_height);
+	t[0].data = (int *)mlx_get_data_addr(t[0].img, \
+			&t[0].bits_per_pixel, &t[0].size_line, \
+			&t[0].endian);
+	t[1].img = mlx_xpm_file_to_image(c->mlx_ptr, c->drct.south_path, \
+			&t[1].img_width, &t[1].img_height);
+	t[1].data = (int *)mlx_get_data_addr(t[1].img, \
+			&t[1].bits_per_pixel, &t[1].size_line, \
+			&t[1].endian);
+	t[2].img = mlx_xpm_file_to_image(c->mlx_ptr, c->drct.east_path, \
+			&t[2].img_width, &t[2].img_height);
+	t[2].data = (int *)mlx_get_data_addr(t[2].img, \
+			&t[2].bits_per_pixel, &t[2].size_line, \
+			&t[2].endian);
+	t[3].img = mlx_xpm_file_to_image(c->mlx_ptr, c->drct.west_path, \
+			&t[3].img_width, &t[3].img_height);
+	t[3].data = (int *)mlx_get_data_addr(t[3].img, \
+			&t[3].bits_per_pixel, \
+			&t[3].size_line, &t[3].endian);
+	if (!t[0].img || !t[1].img || !t[2].img || \
+			!t[3].img)
+		return ;
+	if (!t[0].data || !t[1].data || !t[2].data || \
+			!t[3].data)
+		return ;
+	if (t[0].img_width != t[0].img_height || t[1]. \
+			img_width != t[1].img_height || t[2]. \
+			img_width != t[2].img_height || t[3]. \
+			img_width != t[3].img_height)
+		return ;
 }
--- rsc/main.c
+++ rsc/main.c
@@ -6,18 +6,11 @@
 /*   By: yismaili < yismaili@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 16:46:01 by yismaili          #+#    #+#             */
-/*   Updated: 2022/11/28 21:52:03 by yismaili         ###   ########.fr       */
+/*   Updated: 2022/11/28 22:38:45 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
-
-int	ft_close(int keycode)
-{
-	if (keycode)
-		exit(1);
-	return (0);
-}
 
 void	hooking(t_struct *cub)
 {
@@ -29,89 +22,43 @@ void	hooking(t_struct *cub)
 	mlx_loop(cub->mlx_ptr);
 }
 
-int	motion_notify(int x, int y, t_struct *cub)
+int	check_input(t_struct *cub, char **av, int ac)
 {
-	if (x > 0 && x < W_WIDTH && y > 0 && y < W_HEIGHT)
-	{
-		if (cub->med_x < x)
-			cub->player.rottAngle += cub->player.rottSpeed / 6;
-		if (cub->med_x > x)
-			cub->player.rottAngle -= cub->player.rottSpeed / 6;
-		if (cub->med_x == x)
-			cub->player.rottAngle = cub->player.rottAngle;
-		cub->med_x = x;
-	}
-	return (0);
-}
-
-int	typeofmap(char *path, char *type, int len)
-{
-	int	i;
-
-	i = 0;
-	while (path[i])
-	{
-		if (path[i] == '.')
-			break ;
-		i++;
-	}
-	if (ft_strncmp(path + i, type, len))
-		return (-2);
-	return (0);
+	cub->scaleHeight = 64;
+	cub->scaleWidth = 64;
+	cub->texture_height = 64;
+	cub->texture_width = 64;
+	cub->med_x = W_WIDTH / 2;
+	if (ac != 2)
+		return (ft_putstr_fd("Usage : ./cub3D path/to/map.cub", 0), 0);
+	if (typeofmap(av[1], ".cub", 4) == -2)
+		return (ft_putstr_fd("type of map incorrect\n", 0), 0);
+	if (ft_read_maps(av[1], cub) == 0)
+		return (0);
+	ft_jump_lines(cub);
+	if (!ft_check_alltextures(cub))
+		return (0);
+	if (!ft_check_rgb(cub))
+		return (0);
+	if (!ft_check_map(cub))
+		return (0);
+	return (1);
 }
 
 int	main(int ac, char **av)
 {
 	t_struct	cub;
-	int			i;
-	int			j;
 
-	cub.scaleHeight = 64;
-	cub.scaleWidth = 64;
-	cub.texture_height = 64;
-	cub.texture_width = 64;
-	cub.med_x = W_WIDTH / 2;
-	if (ac != 2)
-		return (ft_putstr_fd("Usage : ./cub3D path/to/map.cub", 0), 0);
-	if (typeofmap(av[1], ".cub", 4) == -2)
-		return (ft_putstr_fd("type of map incorrect\n", 0), 0);
-	if (ft_read_maps(av[1], &cub) == 0)
-		return (0);
-	ft_jump_lines(&cub);
-	if (!ft_check_alltextures(&cub))
-		return (0);
-	if (!ft_check_rgb(&cub))
-		return (0);
-	if (!ft_check_map(&cub))
+	if (check_input(&cub, av, ac) == 0)
 		return (0);
 	cub.mlx_ptr = mlx_init();
 	cub.win_ptr = mlx_new_window(cub.mlx_ptr, W_WIDTH, W_HEIGHT, "cub3D");
 	cub.color_buffer = (unsigned int **) ft_calloc(sizeof(unsigned int *), \
 			W_HEIGHT);
-	i = -1;
-	while (++i < W_HEIGHT)
-		cub.color_buffer[i] = (unsigned int *)ft_calloc(sizeof(unsigned int), \
-				W_WIDTH);
-	ft_colorBuffer(&cub);
-	cub.wallTexture = (unsigned int *) ft_calloc (sizeof(unsigned int), \
-			(unsigned int)cub.scaleWidth * (unsigned int)cub.scaleHeight);
-	i = 0;
-	while (i < cub.scaleWidth)
-	{
-		j = 0;
-		while (j < cub.scaleHeight)
-		{
-			if (i % 8 && j % 8)
-				cub.wallTexture[(cub.scaleWidth * j) + i] = 0xadd8e6;
-			else
-				cub.wallTexture[(cub.scaleWidth * j) + i] = 0xFFF0000;
-			j++;
-		}
-		i++;
-	}
 	cub.img = mlx_new_image(cub.mlx_ptr, W_WIDTH, W_HEIGHT);
 	cub.addr = (int *)mlx_get_data_addr(cub.img, &cub.bits_per_pixel, \
 			&cub.line_length, &cub.endian);
+	color_oftexture(&cub);
 	player_position(&cub);
 	directionOfPlayer(&cub);
 	cub.widthofmap = cub.scaleWidth * cub.widthof_minimap;
@@ -120,45 +67,4 @@ int	main(int ac, char **av)
 	ft_draw_map(&cub);
 	hooking(&cub);
 	return (0);
-}
-
-void	init_textures(t_struct *cub)
-{
-	cub->texture = malloc(sizeof(t_textures) * 4);
-	load_texture(cub, cub->texture);
-}
-
-void	load_texture(t_struct *cub, t_textures *texture)
-{
-	texture[0].img = mlx_xpm_file_to_image(cub->mlx_ptr, cub->drct.north_path, \
-			&texture[0].img_width, &texture[0].img_height);
-	texture[0].data = (int *)mlx_get_data_addr(texture[0].img, \
-			&texture[0].bits_per_pixel, &texture[0].size_line, \
-			&texture[0].endian);
-	texture[1].img = mlx_xpm_file_to_image(cub->mlx_ptr, cub->drct.south_path, \
-			&texture[1].img_width, &texture[1].img_height);
-	texture[1].data = (int *)mlx_get_data_addr(texture[1].img, \
-			&texture[1].bits_per_pixel, &texture[1].size_line, \
-			&texture[1].endian);
-	texture[2].img = mlx_xpm_file_to_image(cub->mlx_ptr, cub->drct.east_path, \
-			&texture[2].img_width, &texture[2].img_height);
-	texture[2].data = (int *)mlx_get_data_addr(texture[2].img, \
-			&texture[2].bits_per_pixel, &texture[2].size_line, \
-			&texture[2].endian);
-	texture[3].img = mlx_xpm_file_to_image(cub->mlx_ptr, cub->drct.west_path, \
-			&texture[3].img_width, &texture[3].img_height);
-	texture[3].data = (int *)mlx_get_data_addr(texture[3].img, \
-			&texture[3].bits_per_pixel, \
-			&texture[3].size_line, &texture[3].endian);
-	if (!texture[0].img || !texture[1].img || !texture[2].img || \
-			!texture[3].img)
-		return ;
-	if (!texture[0].data || !texture[1].data || !texture[2].data || \
-			!texture[3].data)
-		return ;
-	if (texture[0].img_width != texture[0].img_height || texture[1]. \
-			img_width != texture[1].img_height || texture[2]. \
-			img_width != texture[2].img_height || texture[3]. \
-			img_width != texture[3].img_height)
-		return ;	
 }
