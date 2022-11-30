74d21788a5fcc8b64e721724f7fc803be8e91776
Younes Ismaili <yismaili@e2r9p8.1337.ma>
2022-11-30 20:00:08 +0100

    fix north derictin of player 'north south west east'

--- raycaster/raycasting_tool.c
+++ raycaster/raycasting_tool.c
@@ -6,7 +6,7 @@
 /*   By: yismaili < yismaili@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 20:03:04 by yismaili          #+#    #+#             */
-/*   Updated: 2022/11/30 17:35:16 by yismaili         ###   ########.fr       */
+/*   Updated: 2022/11/30 19:05:34 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,8 @@ int	check_wall(t_struct *cub, double x, double y)
 		if (cub->my_map[gred_y][gred_x] == '1' \
 		|| cub->my_map[gred_y][gred_x] == ' ')
 			return (1);
-		if (cub->my_map[gred_y][gred_x] == '0')
+		if (cub->my_map[gred_y][gred_x] == '0' \
+		|| ft_isalpha(cub->my_map[gred_y][gred_x]))
 			return (0);
 	}
 	return (1);
